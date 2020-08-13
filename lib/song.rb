53d963ae55949d27e64551257d2211ca544942f2
Chaim Bryski <luzybry94@gmail.com>
2020-08-13 19:58:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
   def self.genre_count
     genre_hash = {}
     @@genres.each do |genre|
-      if genre_hash.keys.include?(genre)
+      if genre_hash[genre]
         genre_hash|genre| += 1
       else
       genre_hash[genre] = 1
